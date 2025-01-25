9cede264027dd764675be6611fc68337fc227e27
Eduard <sory_2018@yahoo.com>
2025-01-25 23:24:07 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ValentinePage from './ValentinePage'; // Adjust the path if necessary
+
+function App() {
+  return (
+    <div>
+      <ValentinePage />
+    </div>
+  );
+}
+
+export default App;
